a3cb5443eac1863453b98a62e564919671107f0a
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2016-09-21 12:58:47 +0200

    Remove python directory

--- python/pipeline.py
+++
@@ -1,6 +0,0 @@
-from pyspark import SparkContext, SparkConf
-from parameter_grid import generate_parameters
-
-if __name__ == '__main__':
-    conf = SparkConf().setAppName('Pipeline').setMaster('local')
-    sc = SparkContext('local[*]', conf=conf)
